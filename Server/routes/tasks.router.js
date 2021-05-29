c6fb1d5e5cc9f87e30454b34d5f4ef44907d8aa3
Brandon Wharton <whartonbm@gmail.com>
2021-05-29 11:18:15 -0500

    Began writing post route in server

--- Server/routes/tasks.router.js
+++ Server/routes/tasks.router.js
@@ -9,13 +9,27 @@ tasksRouter.get('/', (req, res) => {
     let queryText = `SELECT * FROM "todo_list" ORDER BY "urgent" DESC, "id" DESC;`;
     // request table from DB
     pool.query(queryText).then(result => {
-        console.log('GET tasks from DB to Server');
+        console.log('GET tasks from DB to server');
         res.send(result.rows);
     }).catch (err =>{
         console.log('Error with GET in router', err);
         res.sendStatus(500);
     });
 });
+
+// POST
+tasksRouter.post('/', (req, res) => {
+    // query to add a new task to the DB and sanitize values from client
+    let queryText = `INSERT INTO "todo_list"("task", "urgent") VALUES ($1, $2);`;
+    // store values from client
+    let taskToAdd = req.body;
+    let values = [taskToAdd.task, taskToAdd.urgent];
+    // send POST request to DB
+    pool.query(queryText, values).then(result => {
+        console.log('POST a task to DB from server');
+        
+    })
+})
 
 
 
