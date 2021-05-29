133cd1b13447e2b8106c60a5eef3b0d1873f4bad
Brandon Wharton <whartonbm@gmail.com>
2021-05-28 20:27:04 -0500

    Fixed typo

--- Server/public/scripts/client.js
+++ Server/public/scripts/client.js
@@ -31,9 +31,8 @@ function renderList(taskArray) {
     // append all tasks to DOM in order received from DB
     taskArray.forEach(taskItem => {
         el.append(`
-        
         <li>
-            <button type="button" class="btn btn-success completeBtn" data-id="${tastItem.id}>Complete Task</button>
+            <button type="button" class="btn btn-success completeBtn" data-id="${taskItem.id}">Complete Task</button>
             ${taskItem.task}
             <input class="form-check-input urgentItemCheckbox" type="checkbox" 
             id="${taskItem.id}" data-id="${taskItem.id}">
